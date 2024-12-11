412dcd244b62a46a44135928d580ed2b7be20196
Srdjan Bocanji <srdjanbocanji@Srdjans-MacBook-Pro.local>
2024-12-11 23:45:06 +0100

    initial commit

---
+++ src/test/java/com/bocanjis/rag_demo/RagDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bocanjis.rag_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
